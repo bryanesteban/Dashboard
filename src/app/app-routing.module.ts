8247e92f7e4a688b1e73b178324f9d4a488bf985
bryanesteban <bryanesteban_123@hotmail.com>
2024-05-17 16:02:57 -0500

    routing of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,61 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+    children: [
+      {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component').then( m => m.ChangeDetectionComponent),
+      },
+      {
+        path: 'control-flow',
+        title: 'Control Flow',
+        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component').then(m => m.ControlFlowComponent),
+      },
+      {
+        path: 'defer-options',
+        title: 'Defer Options',
+        loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component').then(m => m.DeferOptionsComponent),
+      },
+      {
+        path: 'defer-views',
+        title: 'Defer Views',
+        loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component').then(m => m.DeferViewsComponent),
+      },
+      {
+        path: 'user/:id',
+        title: 'User View',
+        loadComponent: () => import('./dashboard/pages/user/user.component').then(m => m.UserComponent),
+      },
+      {
+        path: 'user-list',
+        title: 'User List',
+        loadComponent: () => import('./dashboard/pages/users/users.component').then(m => m.UsersComponent),
+      },
+      {
+        path: 'view-transition',
+        title: 'View Transition',
+        loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component').then(m => m.ViewTransitionComponent),
+      },
+      {
+        path:'', redirectTo: 'control-flow', pathMatch: 'full',
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
