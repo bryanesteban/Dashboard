31024ba3fdbd9081eb59cd7875102437f290b22b
bryanesteban <bryanesteban_123@hotmail.com>
2024-05-19 05:27:39 -0500

    menu bar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
-  imports: [CommonModule],
+  imports: [RouterModule, SideMenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``,
   standalone:true
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { routes } from '../../app-routing.module';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
+  imports: [CommonModule],
   styleUrl: './side-menu.component.css',
   standalone:true
 })
 export class SideMenuComponent {
 
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter( route => route && route.path)
+    .filter( route => !route.path?.includes(':'))
+
+
+
+
+  constructor(){
+    // const dashBoardRoutes = routes
+    // .map(route => route.children ?? [])
+    // .flat()
+    // .filter( route => route && route.path)
+    // .filter( route => !route.path?.includes(':'))
+
+
+    // console.log(dashBoardRoutes);
+  }
 }
